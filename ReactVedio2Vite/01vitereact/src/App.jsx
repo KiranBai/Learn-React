dd06b3f65ddd51fd2400d8e142a08238f1a1a5cb
KiranBai <kiranbai10091999@gmail.com>
2024-09-01 01:16:26 +0500

    Learning React

--- ReactVedio2Vite/01vitereact/src/App.jsx
+++ ReactVedio2Vite/01vitereact/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Btywise from './Btywise';
 
 function App() {
   
 
   return (
+  <>
  <h1>Hello Kiran React Learning</h1>
+ <Btywise/>
+ </>
   );
 }
 
---
+++ ReactVedio2Vite/01vitereact/src/Btywise.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function Btywise() {
+  
+
+  return (
+ <h1>Bytwise React Learning</h1>
+  );
+}
+
+export default Btywise
+
+
+// Component name must be captilize
